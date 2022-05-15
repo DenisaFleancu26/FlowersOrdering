93284eb109a18d41a3fa6f0710d652999d81ac82
AleGabriela15 <alexandra.g.fratila@gmail.com>
2022-05-15 14:38:39 +0300

    Log In add function to distinct Manager from Customer.
    https://github.com/DenisaFleancu26/FlowersOrdering.git

--- src/main/java/org/loose/fis/sre/controllers/LoginController.java
+++ src/main/java/org/loose/fis/sre/controllers/LoginController.java
@@ -13,6 +13,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.loose.fis.sre.exceptions.InvalidValidationException;
+import org.loose.fis.sre.services.UserManagerService;
 import org.loose.fis.sre.services.UserService;
 import javafx.scene.control.Button;
 
@@ -47,10 +48,36 @@ public class LoginController {
     }
 
     @FXML
-    public void handleLoginAction() {
+    public void handleLoginAction(javafx.event.ActionEvent event) throws Exception {
         try {
-            int ok= UserService.checkInvalidLogin(usernameField.getText(), passwordField.getText(), (String) role.getValue());
-            if(ok==0) throw new InvalidValidationException();
+
+
+            if (  ((String) role.getValue() )== "Customer") {
+
+                int ok= UserService.checkInvalidLogin(usernameField.getText(), passwordField.getText(), (String) role.getValue());
+                if(ok==0) throw new InvalidValidationException();
+
+                Stage stage = new Stage();
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("MainPageCustomer.fxml"));
+                Pane root = fxmlLoader.load();
+                ((Node) (event.getSource())).getScene().getWindow().hide();
+                stage.setTitle("Flowers Ordering (Customer)");
+                stage.setScene(new Scene(root, 1200, 700));
+                stage.show();
+            }
+            else {
+                int ok = UserManagerService.checkInvalidLogin(usernameField.getText(), passwordField.getText(), (String) role.getValue());
+                if (ok == 0) throw new InvalidValidationException();
+
+                Stage stage = new Stage();
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("MainPageManager.fxml"));
+                Pane root = fxmlLoader.load();
+                ((Node) (event.getSource())).getScene().getWindow().hide();
+                stage.setTitle("Flowers Ordering (Manager)");
+                stage.setScene(new Scene(root, 1200, 700));
+                stage.show();
+            }
+
         } catch (InvalidValidationException e) {
             loginMessage.setText(e.getMessage());
         }
